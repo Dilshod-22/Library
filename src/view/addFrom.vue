0e66cdb758466aeda7afa57f73bcbd9d4669c135
Dilshod-22 <myprofileforgithu@gmail.com>
2023-06-06 11:17:00 +0500

    yangilandi

--- src/view/addFrom.vue
+++ src/view/addFrom.vue
@@ -7,7 +7,8 @@
         </label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="title" type="text" @input="(e) => this.info.title = e.target.value" placeholder="Title" required />
+          id="title" type="text" :value="info.title" @input="(e) => this.info.title = e.target.value" placeholder="Title"
+          required />
       </div>
       <div class="mb-2">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="author">
@@ -15,14 +16,15 @@
         </label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="author" @input="(e) => this.info.author = e.target.value" type="text" placeholder="Author" required />
+          id="author" :value="info.author" @input="(e) => this.info.author = e.target.value" type="text"
+          placeholder="Author" required />
       </div>
       <div class="mb-6">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="isbn">
           ISBN#
         </label>
         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight" id="isbn"
-          @input="(e) => this.info.isbn = e.target.value" placeholder="ISBN#" required />
+          :value="info.isbn" @input="(e) => this.info.isbn = e.target.value" placeholder="ISBN#" required />
       </div>
       <div class="flex items-center justify-between">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-auto" type="submit"
@@ -47,17 +49,19 @@ export default {
   },
   methods: {
     sendItem() {
-      let id = Math.floor(Math.random() * 9999)
-      let item = {
-        title: this.info.title,
-        author: this.info.author,
-        isbn: this.info.isbn,
-        id: id
+      if (title && author && isbn) {
+        let id = Math.floor(Math.random() * 9999)
+        let item = {
+          title: this.info.title,
+          author: this.info.author,
+          isbn: this.info.isbn,
+          id: id
+        }
+        this.$emit('addItemHandler', item)
+        this.info.title = " "
+        this.info.author = " "
+        this.info.isbn = " "
       }
-      this.$emit('addItemHandler', item)
-      this.info.title = " "
-      this.info.author = " "
-      this.info.isbn = " "
     }
   }
 };
